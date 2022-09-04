908091bedad9bb8e935893d9f14d54e2d2b43ede
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2022-09-04 07:01:46 +0300

    update index.tsx
    Lorem added by Feruz

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,6 +13,12 @@ export default function Home() {
       <h2>
         Skill Test (Software Engineer)
       </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ea,
+        sapiente at exercitationem nihil, molestiae laboriosam tempore, in
+        deserunt vel obcaecati. Pariatur debitis maiores libero, atque quis
+        minus nisi commodi.
+      </p>
     </div>
   );
 }

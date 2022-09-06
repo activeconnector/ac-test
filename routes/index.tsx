5e5d322a515eba0a7ce3a1ec90adf3d6324caa0d
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2022-09-06 15:11:29 +0300

    fetched some posts

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,19 @@
 /** @jsx h */
 import { h } from "preact";
 
-export default function Home() {
+export const handler: Handlers<Project> = {
+  async GET(_req, ctx) {
+    const myposts =await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await myposts.json()
+    if (!posts) {
+      return new Response("Project not found", { status: 404 });
+    }
+    return ctx.render(posts);
+  },
+};
+
+export default function Home(props) {
+  const posts = props.data
   return (
     <div>
       <a href="https://www.active-connector.com/">
@@ -13,6 +25,11 @@ export default function Home() {
       <h2>
         Skill Test (Software Engineer)
       </h2>
+      <ul>
+        {posts.map(post => {
+          return <li><a href={`/posts/${post.id}`}>{post.title}</a></li>
+        })}
+      </ul>
     </div>
   );
 }

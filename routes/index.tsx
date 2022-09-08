7fb8a3b0b8094c17076c718ee876c1ff3e942ab3
Alan <tech@alan.olrun.io>
2022-09-08 13:54:10 +0900

    Revert "Format HTML & JS codes"
    This reverts commit afab101cf7d381246f1d3db8744804d518edafdb.

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,34 +3,37 @@ import { h } from "preact";
 
 export default function Home() {
   return (
-<div>
-   <a href="https://www.active-connector.com/">
-   <img
-      src="/logo.png"
-      alt="Active Connector company logo"
-      />
-   </a>
-   <h2>
-      Skill Test (Software Engineer)
-   </h2>
-   <p>
-      <input type="text" id="jobInput"/>
-   </p>
-   <button id="search" onclick="search()">Search</button>
-   <script>
-      function search(){
-         var input = document.getElementById("jobInput").value
+    <div>
+      <a href="https://www.active-connector.com/">
+        <img
+          src="/logo.png"
+          alt="Active Connector company logo"
+        />
+      </a>
+      <h2>
+        Skill Test (Software Engineer)
+      </h2>
+
+        <p>
+          <input type="text" id="jobInput"/>
+        </p>
+        <button id="search" onclick="search()">Search</button>
       
-         if (input == "engineer") {
-          document.getElementById("div").innerText = "Job \"engineer\" is open for you!"
-         } else {
-          document.getElementById("div").innerText = "Job \"" + input + "\" is not available"
-         }
-      
-         location.replace("http://localhost:8000/jobs/" + input)  
-      }
-   </script>
-</div>
+
+      <script>
+        function search(){
+           var input = document.getElementById("jobInput").value
+
+           if (input == "engineer") {
+            document.getElementById("div").innerText = "Job \"engineer\" is open for you!"
+           } else {
+            document.getElementById("div").innerText = "Job \"" + input + "\" is not available"
+           }
+        
+           location.replace("http://localhost:8000/jobs/" + input)  
+        }
+      </script>
+    </div>
 
   );
 }

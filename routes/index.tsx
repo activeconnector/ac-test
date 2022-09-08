32ef88b07e560def841f8cba9c41792b3e32c247
Alan <tech@alan.olrun.io>
2022-09-08 11:03:03 +0900

    Add back origianl deno test and try a new HTML format

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ import { h } from "preact";
 
 export default function Home() {
   return (
-<> 
+<>
 <div>
    <a href="https://www.active-connector.com/">
    <img
@@ -18,20 +18,20 @@ export default function Home() {
       <input type="text" id="jobInput"/>
    </p>
    <button id="search" onclick="search()">Search</button>
-   <script>
-      function search(){
-         var input = document.getElementById("jobInput").value
-      
-         if (input == "engineer") {
-          document.getElementById("div").innerText = "Job \"engineer\" is open for you!"
-         } else {
-          document.getElementById("div").innerText = "Job \"" + input + "\" is not available"
-         }
-      
-         location.replace("http://localhost:8000/jobs/" + input)  
-      }
-   </script>
 </div>
+<script>
+   function search(){
+      var input = document.getElementById("jobInput").value
+   
+      if (input == "engineer") {
+       document.getElementById("div").innerText = "Job \"engineer\" is open for you!"
+      } else {
+       document.getElementById("div").innerText = "Job \"" + input + "\" is not available"
+      }
+   
+      location.replace("http://localhost:8000/jobs/" + input)  
+   }
+</script>
 </>
 
   );

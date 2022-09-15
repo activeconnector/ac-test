2323c65662d195cd609fd5e980740f3fc978330c
Kobe Ruado <insidiae423@gmail.com>
2022-09-15 19:48:30 +0800

    Add main logic for skill test

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,21 @@
 /** @jsx h */
 import { h } from "preact";
 
-export default function Home() {
+import type { Handlers, PageProps } from "$fresh/server.ts";
+
+export const handler: Handlers<string> = {
+  async POST(req, ctx) {
+    const formData = await req.formData();
+
+    const job = formData.get("job") as string;
+
+    return job
+      ? Response.redirect(`http://localhost:8000/jobs/${job}`)
+      : ctx.render("error: empty input");
+  },
+};
+
+export default function Home({ data }: PageProps<string>) {
   return (
     <div>
       <a href="https://www.active-connector.com/">
@@ -13,6 +27,13 @@ export default function Home() {
       <h2>
         Skill Test (Software Engineer)
       </h2>
+
+      <form method="POST">
+        <input type="text" id="job" name="job" />
+        <button type="submit">Search Job</button>
+      </form>
+
+      {data ? <p>{data}</p> : null}
     </div>
   );
 }
